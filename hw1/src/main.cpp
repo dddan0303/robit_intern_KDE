6a243d634d9dc3ec81849bbeef2f6219a206d9cb
dddan0303 <dddan0303@gmail.com>
2025-09-04 14:53:52 +0900

    Add files via upload

---
+++ hw1/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/hw1/hw1.hpp"
+using namespace std;
+
+int main() {
+    ArrayCalculator calculator;
+    calculator.inputArraySize();
+    calculator.inputArrayValues();
+    calculator.displayResults();
+    return 0;
+}
